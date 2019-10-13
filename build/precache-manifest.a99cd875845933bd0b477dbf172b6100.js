a704a30bb1f95d955c3a8d540896c3d331f3021c
gcarver <gcarver@gcarvers-MacBook-Pro.local>
2019-10-13 08:52:07 -0500

    Oath

---
+++ build/precache-manifest.a99cd875845933bd0b477dbf172b6100.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de027644dd968dbc68f1e98611a0d401",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cb62a63822a3292f8c4",
+    "url": "/static/css/main.319bac23.chunk.css"
+  },
+  {
+    "revision": "df73e091e312053846be",
+    "url": "/static/js/2.6d38c2fa.chunk.js"
+  },
+  {
+    "revision": "8cb62a63822a3292f8c4",
+    "url": "/static/js/main.a6b0ca14.chunk.js"
+  },
+  {
+    "revision": "d0243843e604c61af12f",
+    "url": "/static/js/runtime-main.6887b7c4.js"
+  }
+]);
--- src/Pages/App/App.js
+++ src/Pages/App/App.js
@@ -29,7 +29,7 @@ class App extends Component {
 render() {
   return (
     <div>
-      <h1>Bargain</h1>
+      <h1>Cobra</h1>
       <Switch>
         <Route exact path='/' render={() =>
           <NavBar 
